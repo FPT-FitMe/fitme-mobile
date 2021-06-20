ffccb63168391c44b71107d73c1139dbadd4012a
Ngô Tấn Đức <ngotanduc410@gmail.com>
2021-06-20 23:45:34 +0700

    Remove unused images, refractor ui at NewUserInfo

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -19,7 +19,6 @@ import 'package:fitme/screens/DetailPracticeScreen/practice_set.dart';
 import 'package:fitme/screens/DetailPracticeScreen/practice_success.dart';
 import 'package:fitme/screens/RegisterScreen/register.dart';
 import 'package:fitme/screens/NewUserInfoScreen/new_user_info.dart';
-import 'package:fitme/screens/SearchScreen/search.dart';
 import 'package:fitme/screens/SettingScreen/setting.dart';
 import 'package:fitme/screens/SurveyScreen/survey.dart';
 import 'package:fitme/screens/UserProfileScreen/user_profile.dart';
--- lib/screens/NewUserInfoScreen/new_user_info.dart
+++ lib/screens/NewUserInfoScreen/new_user_info.dart
@@ -8,52 +8,56 @@ class NewUserInfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.only(left: 20, right: 20),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(height: 50),
-                  Text(
-                    "Xin chào Tùng,",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 20),
-                  Text("Để bắt đầu, chúng tôi cần một số thông tin của bạn",
-                      style: TextStyle(fontSize: 16, color: AppColors.grayText)),
-                  SizedBox(height: 40),
-                  SvgPicture.asset("assets/images/newUserInfo.svg",
-                      height: 350,
-                      width: 350,
-                      fit: BoxFit.contain),
-                  SizedBox(height: 75),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 55,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, AppRoutes.survey);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                      child: Text(
-                        "Bắt đầu",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 20),
-                      ),
-                    ),
-                  ),
-                ],
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: SizedBox(
+          width: double.infinity,
+          height: 55,
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, AppRoutes.survey);
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
               ),
             ),
+            child: Text(
+              "Bắt đầu",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20),
+            ),
           ),
-      )
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: 50),
+                Text(
+                  "Xin chào Tùng,",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Để bắt đầu, chúng tôi cần một số thông tin của bạn",
+                  style: TextStyle(fontSize: 16, color: AppColors.grayText),
+                ),
+                SizedBox(height: 20),
+                SvgPicture.asset(
+                  "assets/images/new_user_info.svg",
+                  fit: BoxFit.contain,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
